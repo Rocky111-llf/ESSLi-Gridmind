07e54f8e12382e7aec56fc30b41b50c7605d0458
Rocky111-llf <1114870068@qq.com>
2024-09-05 17:15:50 +0800

    ✨ 完成跟网的PQ控制

--- 4.AlgoComm/AlgoComm.c
+++ 4.AlgoComm/AlgoComm.c
@@ -83,10 +83,7 @@ void PIDProc_Int_Sepa(PID* l_PID)
     //积分控制：仅在使用积分分离时应用 
 	if(((l_PID->UF==-1)&&(l_PID->Err>0.0f))||((l_PID->UF==1)&&(l_PID->Err<0.0f))||l_PID->UF==0){
 		if(fabs(l_PID->Err) > ERR_THRE){
-			debug2 += 1;
 			l_PID->I += l_PID->Err * l_PID->Ki; // 仅当误差大于阈值时，累积积分项
-		}else{
-			debug1 += 1;
 		}
 	}
 	// 计算控制输出
